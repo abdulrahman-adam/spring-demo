cf889caa0a5bf0ac522541836700f92fd6ec1d95
abdulrahman <alshatyr@hotmail.com>
2024-08-06 14:30:50 +0200

    initializing spring boot project

---
+++ demoSpring/src/test/java/com/ayamultipedia/demoSpring/DemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayamultipedia.demoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
